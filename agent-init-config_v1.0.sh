6b5915462fd78c90b25498cc07329bd0834fa9e9
RyuS3ki <100291649@alumnos.uc3m.es>
2017-03-02 11:39:23 +0100

    Empezado el nuevo fix

---
+++ agent-init-config_v1.0.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#Sustituimos la configuracion generica por la propia
+cp /usr/dist/ambari/ambari-etl/ambari-agent.ini /etc/ambari-agent/conf/ambari-agent.ini
+
+#Añadimos el equipo a /etc/hosts
+python hosts-ambari.py
+
+
