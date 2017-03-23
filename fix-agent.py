8e75bfd782419d6bed18bc07937771ccfceec73c
RyuS3ki <100291649@alumnos.uc3m.es>
2017-03-23 14:00:48 +0100

    Script con getopt funcionando

--- fix-agent.py
+++ fix-agent.py
@@ -1,46 +1,82 @@
-#!/bin/python
+#!/usr/bin/env python
 
-""""
-Script en python para la configuración inicial de los agentes de Ambari. Debe
-lanzarse antes de la creación del cluster y cogerá como argumentos los servicios
+"""
+Script en python para la configuracion inicial de los agentes de Ambari. Debe
+lanzarse antes de la creacion del cluster y cogera como argumentos los servicios
 que se van a instalar en el cliente concreto donde se lanza.
-El código está organizado en funciones según los servicios para los que se van a
+El codigo esta organizado en funciones segun los servicios para los que se van a
 efectuar los cambios.
-""""
+"""
 
 import os
 import subprocess
 import sys
 import getopt
 
-#Código para leer argumentos
+#DataNode, NameNode, NodeManager
+#def Generic():    
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 's:h', ['', 'services=', 'help'])
-except getopt.GetoptError:
-    usage()
-    sys.exit(2)
+#def HDFS():
 
-for opt, arg in opts:
-    if opt in ('-h', '--help'):
+#def Yarn():
+
+#def MapReduce():
+
+#def Tez():
+
+#def Hive():
+
+#def HBase():
+
+#def Pig():
+
+#def ZooKeeper():
+
+#def AmbariMetrics():
+
+#def Spark():
+
+#def all_services():
+
+def usage():
+  print "\nThis is the usage function\n"
+  print 'Usage: '+sys.argv[0]+' -[option]'
+  print '-a:                        All services are configured'
+  print '-s path/to/file.conf:      You must specify a config file with a list of services'
+  print '-h:                        This help is printed'
+
+#Codigo para leer argumentos
+def main(argv):
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'ahs:', ['all', 'services=', 'help'])
+    except getopt.GetoptError:
         usage()
         sys.exit(2)
-    elif opt in ('-s', '--services'):
-        servicesList = ast.literal_eval(sys.argv[2])
-        all_arg = 0
-    else:
-        print 'Se harán cambios para todos los servicios'
-        cont = input("¿Está seguro? (y/n):")
-        if cont == y:
-            all_arg = 1
-        elif cont == n:
-            all_arg = 0
-            print 'Lance otra vez el fix e introduzca los parámetros deseados'
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
             usage()
             sys.exit(2)
+        elif opt in ('-s', '--services'):
+            print 'Abriendo archivo de configuracion'
+            confile = sys.argv[2]
+        elif opt in ('-a', '--all'):
+            print 'Se haran cambios para todos los servicios'
+            cont = raw_input("Esta seguro? (y/n):")
+            if cont == "y":
+                all_services()
+            elif cont == "n":
+                print 'Lance otra vez el fix e introduzca los parametros deseados'
+                usage()
+                sys.exit(2)
+            else:
+                print 'Valor erroneo, debe introducir y/n'
+                usage()
+                sys.exit(2)
         else:
-            print 'Valor erróneo, debe introducir y/n'
             usage()
             sys.exit(2)
 
-def HCat():
+#Ejecucion del main
+if __name__ =='__main__':
+    main(sys.argv[1:])
