40f5f02953c7cf046cfe29776da903f55baa3965
RyuS3ki <100291649@alumnos.uc3m.es>
2017-03-23 11:45:29 +0100

    Corregido el agent-init

--- agent-init-config_v1.0.sh
+++ agent-init-config_v1.0.sh
@@ -4,8 +4,4 @@
 cp /usr/dist/ambari/ambari-etl/ambari-agent.ini /etc/ambari-agent/conf/ambari-agent.ini
 
 #Añadimos el equipo a /etc/hosts
-<<<<<<< HEAD
-python hosts_ambari.py
-=======
 python hosts-ambari.py
->>>>>>> fe81879b840e48ac997b4c1bf5a8b44025e2854d
