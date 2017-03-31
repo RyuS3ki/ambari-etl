c734651893c11a836d99c471463bf10e445a3e04
RyuS3ki <100291649@alumnos.uc3m.es>
2017-03-31 12:50:21 +0200

    Config file hecho y parser

--- fix-agent.py
+++ fix-agent.py
@@ -2,10 +2,13 @@
 
 """
 Script en python para la configuracion inicial de los agentes de Ambari. Debe
-lanzarse antes de la creacion del cluster y cogera como argumentos los servicios
+lanzarse antes de la creacion del cluster y puede pasarse como argumento un
+fichero de configuracion con booleanos que indiquen los servicios
 que se van a instalar en el cliente concreto donde se lanza.
 El codigo esta organizado en funciones segun los servicios para los que se van a
 efectuar los cambios.
+Debe ejecutarse siempre, ya que hay servicios genericos que deben prepararse
+siempre.
 """
 
 import os
@@ -22,17 +25,18 @@ def copy(src, dest):
     except OSError as e:
         # If the error was caused because the source wasn't a directory
         if e.errno == errno.ENOTDIR:
-            shutil.copy(src, dest)
+            try:
+                shutil.copy(src, dest)
+            except OSError as e:
+                print('File not copied. Error: %s' % e)
+
         else:
             print('Directory not copied. Error: %s' % e)
 
-#DataNode, NameNode, NodeManager
-def Generic():
+def HDFS():
     copy('/usr/hdp', 'var/bigdata')
     os.rename('/usr/hdp', '/usr/hdp-orig')
     os.symlink('/var/bigdata/hdp', '/usr/hdp')
-
-#def HDFS():
 
 #def Yarn():
 
@@ -52,6 +56,10 @@ def Generic():
 
 #def Spark():
 
+#DataNode, NameNode, NodeManager
+def Generic():
+    HDFS()
+
 def all_services():
     Generic()
     # HDFS()
@@ -65,17 +73,30 @@ def all_services():
     # AmbariMetrics()
     # Spark()
 
+def selector():
+    yarn = Config.getboolean("Services", "Yarn")
+    mreduce = Config.getboolean("Services", "MapReduce")
+    tez = Config.getboolean("Services", "Tez")
+    hive = Config.getboolean("Services", "Hive")
+    hbase = Config.getboolean("Services", "HBase")
+    pig = Config.getboolean("Services", "Pig")
+    zkeeper = Config.getboolean("Services", "ZooKeeper")
+    ams = Config.getboolean("Services", "AmbariMetrics")
+    spark = Config.getboolean("Services", "Spark")
+
+
+
 def usage():
   print "\nThis is the usage function\n"
   print 'Usage: '+sys.argv[0]+' -[option]'
   print '-a:                        All services are configured'
-  print '-s path/to/file.conf:      You must specify a config file with a list of services'
+  print '-s path/to/file.ini:       You must specify a config file with a list of services'
   print '-h:                        This help is printed'
 
 #Codigo para leer argumentos
 def main(argv):
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'ahs:', ['all', 'services=', 'help'])
+        opts, args = getopt.getopt(sys.argv[1:], 'ahs:', ['all', 'help', 'services='])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -87,6 +108,7 @@ def main(argv):
         elif opt in ('-s', '--services'):
             print 'Abriendo archivo de configuracion'
             confile = sys.argv[2]
+            selector(confile)
         elif opt in ('-a', '--all'):
             print 'Se haran cambios para todos los servicios'
             cont = raw_input("Esta seguro? (y/n):")
