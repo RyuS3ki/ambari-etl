b3bae47adf99374902b0b7ad2a8b097b5c6390bd
Ryu_S3ki <rbe.jz.gn@gmail.com>
2017-04-20 13:11:08 +0200

    Delete agent-init-config_v1.0.sh

--- agent-init-config_v1.0.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#Sustituimos la configuracion generica por la propia
-cp /usr/dist/ambari/ambari-etl/ambari-agent.ini /etc/ambari-agent/conf/ambari-agent.ini
-
-#Añadimos el equipo a /etc/hosts
-python hosts-ambari.py
-
-
