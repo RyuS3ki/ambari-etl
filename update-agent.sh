267efd933cfa37c1158b2ec9c73f06a5f7fa0610
RyuS3ki <100291649@alumnos.uc3m.es>
2017-05-11 11:51:16 +0200

    Atomizando cosas del reseteo

--- update-agent.sh
+++ update-agent.sh
@@ -6,3 +6,7 @@ mount -o remount,rw /usr
 dpkg -i /usr/dist/ambari/ambari-packages/ambari-agent*
 mkdir /usr/hdp
 mount -o remount,ro /usr
+
+ambari-agent stop
+sh /var/bigdata/ambari-etl/agent-init-config_v1.0.sh
+ambari-agent start
