1331090af7ff69713b67d0b07b7d8286778f7411
RyuS3ki <100291649@alumnos.uc3m.es>
2017-05-15 10:15:03 +0200

    Añadiendo directorios que hay que purgar al uninstall

--- ambari-agent-uninstall.sh
+++ ambari-agent-uninstall.sh
@@ -8,9 +8,10 @@ rm -rf /var/lib/ambari-agent
 rm -rf /var/run/ambari-agent
 rm -rf /usr/lib/ambari-agent
 rm -rf /usr/lib/python2.6/site-packages/ambari-agent
-rm -rf /usr/hdp
 rm -rf /var/bigdata/servicios
 rm -rf /var/bigdata/backup
+rm -rf /usr/hdp
+rm -rf /etc/ambari-agent
 
 dpkg --purge ambari-agent
 
