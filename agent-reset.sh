5c748945f620097f334d2527263af8b9989fbb6b
RyuS3ki <100291649@alumnos.uc3m.es>
2017-05-15 10:11:05 +0200

    Probando la funcion reset

--- agent-reset.sh
+++ agent-reset.sh
@@ -8,5 +8,7 @@ rm -rf /var/lib/ambari-agent
 rm -rf /var/run/ambari-agent
 rm -rf /usr/lib/ambari-agent
 rm -rf /usr/lib/python2.6/site-packages/ambari-agent
+rm -rf /usr/hdp
+rm -rf /var/bigdata/servicios
 
 mount -o remount,ro /usr
--- fix-agent.py
+++ fix-agent.py
@@ -591,8 +591,10 @@ def selector(confile):
 # Function that resets all client configuration about Ambari (this is the
 # desperate method for "fixing" things)
 def Reset():
-    script = 'agent-reset.sh'
+    restart = 'update-agent'
+    remove = 'ambari-agent-uninstall.sh'
     print "Resetting agent..."
+    subprocess.check_output(remove, shell=True)
     subprocess.check_output(script, shell=True)
     print "Done! Remember to run this script again before connecting to the server"
 
