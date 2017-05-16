5eacaa53aa7336a4da08e133888121a755a0993a
RyuS3ki <100291649@alumnos.uc3m.es>
2017-05-16 10:07:54 +0200

    Añadida la restauracion de hdp-select (enlace simbolico) en el update

--- update-agent.sh
+++ update-agent.sh
@@ -4,7 +4,8 @@ ambari-agent stop
 
 mount -o remount,rw /usr
 dpkg -i /usr/dist/ambari/ambari-packages/ambari-agent*
-mkdir /usr/hdp
+mkdir /usr/hdp/current
+ln -sfn /usr/local/hadoop-2.7.1.2.3.4.0-3347 /usr/hdp/current/hadoop-client
 mount -o remount,ro /usr
 
 ambari-agent stop
