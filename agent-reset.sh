d5199c7e71907dbcb29be4ec68b07fd53ae29c67
RyuS3ki <100291649@alumnos.uc3m.es>
2017-05-16 10:28:14 +0200

    Mejorado el reset+

--- agent-reset.sh
+++ agent-reset.sh
@@ -18,6 +18,7 @@ rm -rf /etc/ambari-agent
 
 echo "Restoring symlinks..."
 
+mkdir /usr/hdp
 mkdir /usr/hdp/current
 ln -sfn /usr/local/hadoop-2.7.1.2.3.4.0-3347 /usr/hdp/current/hadoop-client
 sh /var/bigdata/ambari-etl/agent-init-config_v1.0.sh
